f2d5581615c343b47a5847ed3d9883b76da2d790
ZYXBoy <127312558+ZYXBoy@users.noreply.github.com>
2023-06-14 18:33:24 +0700

    BFSTraversal.java

--- BFSTraversal/src/bfstraversal/BFSTraversal.java
+++ BFSTraversal/src/bfstraversal/BFSTraversal.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package bfstraversal;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- *
- * @author FALL-NOU
- */
 public class BFSTraversal {
     private int node;
     private LinkedList<Integer> adj[];
